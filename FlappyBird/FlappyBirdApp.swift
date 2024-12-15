27327ed59a85f9c73d5f5126d3cb79b409e9feed
Om Preetham Bandi <ompreethambandi@gmail.com>
2024-12-15 01:57:09 -0600

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Om Preetham Bandi on 12/15/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlappyBirdApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FlappyBirdTests/FlappyBirdTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlappyBirdTests.swift
+//  FlappyBirdTests
+//
+//  Created by Om Preetham Bandi on 12/15/24.
+//
+
+import Testing
+
+struct FlappyBirdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
